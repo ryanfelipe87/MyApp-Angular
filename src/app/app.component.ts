80c4f3ecbcbfe5f693fcee1e52e13bb9da49c5ba
Ryan <ryanfelipe.rr@gmail.com>
2024-01-18 17:31:54 -0300

    Learning Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<h1>Hello World</h1>',
+  imports: [HomeComponent, CommonModule, RouterOutlet],
+  template: `<main>
+              <header class="brand-name">
+                <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+              </header>
+              <section class="content">
+                <app-home></app-home>
+              </section>
+            </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template:`<section>
+              <form>
+                <input type="text" placeholder="Filter by City">
+                <button class="primary" type="button">Search</button>
+              </form>
+            </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
