f5afc5b8584726a7a9e3e3fa79ce6048ccd732ce
Ryan <ryanfelipe.rr@gmail.com>
2024-01-18 18:06:56 -0300

    Created housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+            HousingLocationComponent
+          ],
   template:`<section>
               <form>
                 <input type="text" placeholder="Filter by City">
                 <button class="primary" type="button">Search</button>
               </form>
             </section>
+            <section>
+              <app-housing-location></app-housing-location>
+            </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
