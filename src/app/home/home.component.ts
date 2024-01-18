606eb0a297b123ed8b4f01aa1ae1a48b3e6f9177
Ryan <ryanfelipe.rr@gmail.com>
2024-01-18 18:38:36 -0300

    Creating interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -21,5 +22,17 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl = "https://angular.io/assets/images/tutorials/faa";
+
+  housingLocation: Housinglocation ={
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
