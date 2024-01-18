a56d387eb05eaa5b7b52f2c0b26da22ef12a0009
Ryan <ryanfelipe.rr@gmail.com>
2024-01-18 19:32:13 -0300

    Adding input to components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -12,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: Housinglocation;
 }
