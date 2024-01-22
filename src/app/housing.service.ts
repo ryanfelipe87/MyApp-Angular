d8f4bed4bd57e3eb90a8bbf9c0ba9f5aaf63b8d4
Ryan <ryanfelipe.rr@gmail.com>
2024-01-22 08:25:24 -0300

    Angular service

---
+++ src/app/housing.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Housinglocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+
+  housingLocationList: Housinglocation[] = [];
+
+  constructor() { }
+
+  getAllHousingLocations(): Housinglocation[]{
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id:number): Housinglocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+}
