9c9d6283cb9281f6ef5be48e9cb7deb90020ef76
Ryan <ryanfelipe.rr@gmail.com>
2024-01-22 08:44:12 -0300

    Adding routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, CommonModule, RouterOutlet],
+  imports: [HomeComponent, CommonModule, RouterOutlet, RouterModule],
   template: `<main>
-              <header class="brand-name">
-                <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-              </header>
-              <section class="content">
-                <app-home></app-home>
-              </section>
-            </main>`,
+                <a [routerLink]="['/']"]>
+                  <header class="brand-name">
+                  <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+                  </header>
+                </a>
+                <section class="content">
+                  <router-outlet></router-outlet>
+                </section>
+             </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
